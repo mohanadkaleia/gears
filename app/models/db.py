c0f174065187cdc029a1f23668afddde5be370cb
Mohanad Kaleia <ms.kaleia@gmail.com>
2021-07-31 15:13:44 -0700

    decouple the db initialzation from models and prettify the db.json

---
+++ app/models/db.py
@@ -0,0 +1,19 @@
+import tinydb
+
+from tinydb.storages import JSONStorage
+from tinydb_serialization import SerializationMiddleware
+from tinydb_serialization.serializers import DateTimeSerializer
+
+serialization = SerializationMiddleware(JSONStorage)
+serialization.register_serializer(DateTimeSerializer(), "TinyDate")
+
+# DB initializtion
+db = tinydb.TinyDB(
+    "db.json", storage=serialization, sort_keys=True, indent=4, separators=(",", ": ")
+)
+
+shops = db.table("shops")
+services = db.table("services")
+appointments = db.table("appointments")
+promos = db.table("promos")
+vehicles = db.table("vehicles")
